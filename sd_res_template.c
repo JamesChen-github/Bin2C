5e72ac49ab78c5231058c8482f75e8f16dc02a68
陈家皓 <70957022+JamesChen-github@users.noreply.github.com>
2022-09-13 18:23:40 +0800

    Add files via upload

---
+++ sd_res_template.c
@@ -0,0 +1,28 @@
+#include "sdrv_res.h"
+
+#ifndef SDRV_RES_ATTRIBUTE_MEM_ALIGN
+#define SDRV_RES_ATTRIBUTE_MEM_ALIGN
+#endif
+
+#define SDRV_RES_{NAME_UPPER}_RF SDRV_RES_FORMAT_BIN_RAW //SDRV_RES_FORMAT_DATA_RAW
+
+const SDRV_RES_ATTRIBUTE_MEM_ALIGN uint8_t sdrv_res_{NAME_LOWER}_map[] = {
+#if SDRV_RES_{NAME_UPPER}_RF == SDRV_RES_FORMAT_BIN_RAW
+{BIN_RAW}
+#endif
+#if SDRV_RES_{NAME_UPPER}_RF == SDRV_RES_FORMAT_DATA_RAW
+{DATA_RAW}
+#endif
+} __SECTION(".sdrvresdata");
+
+const sdrv_res_dsc_t sdrv_res_{NAME_LOWER} = {
+  .header.always_zero = SDRV_RES_{NAME_UPPER}_RF,
+  .header.rf = 0,
+#if SDRV_RES_{NAME_UPPER}_RF == SDRV_RES_FORMAT_BIN_RAW
+  .data_size = {DATA_SIZE},
+#endif
+#if SDRV_RES_{NAME_UPPER}_RF == SDRV_RES_FORMAT_DATA_RAW
+  .data_size = {DATA_SIZE},
+#endif
+  .data = sdrv_res_{NAME_LOWER}_map,
+};
